0ec049fd942a7408f5018a0c40da10cc754362d7
Cao Sen Miao <caosenmiao@espressif.com>
2021-04-14 20:02:30 +0800

    system_time: add c6 suport

--- components/esp_timer/src/system_time.c
+++ components/esp_timer/src/system_time.c
@@ -30,6 +30,8 @@
 #include "esp32s3/rtc.h"
 #elif CONFIG_IDF_TARGET_ESP32C3
 #include "esp32c3/rtc.h"
+#elif CONFIG_IDF_TARGET_ESP32C6
+#include "esp32c6/rtc.h"
 #endif
 
 // Correction for underlying timer to keep definition
